53dcbcfb8ca3905dadb8b56f7ded4f16030cde31
Fra1994 <80578940+Fra1994@users.noreply.github.com>
2021-04-06 18:27:33 +0200

    Update R_code_remote_sensing_first.r

--- R_code_remote_sensing_first.r
+++ R_code_remote_sensing_first.r
@@ -1,7 +1,9 @@
 # My first code in R for remote sensing
 # Il mio primo codice in R per telerilevamento 
 
-setwd("c:/lab/")
+library(raster)
+
+setwd("C:/lab/")
 
 # install.packages("raster")
 library(raster)
@@ -33,6 +35,8 @@ plot(p224r63_2011$B4_sre, col=cln)
 dev.off ()
 
 plot(p224r63_2011$B1_sre)
+
+
 
 # plot band 1 with a predefinined colut ramp palette
 
@@ -118,4 +122,35 @@ par(mfrow=c(3,1))
 plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="Lin")
 plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="Lin")
 plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="hist")
+
+# Multitemporal set
+p224r63_1988 <- brick("p224r63_1988_masked.grd")
+p224r63_1988
+
+# Bande Landsat
+# B1: blu
+# B2: verde
+# B3: rosso
+# B4: infrarosso vicino
+# B5: infrarosso medio
+# B6: infrarosso termico
+# B7: infrarosso medio
+
+plot(p224r63_1988)
+
+plotRGB(p224r63_1988, r=3, g=2, b=1, stretch="Lin")
+plotRGB(p224r63_1988, r=4, g=3, b=2, stretch="Lin")
+
+par(mfrow=c(2,1))
+plotRGB(p224r63_1988, r=4, g=3, b=2, stretch="Lin")
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
+
+# hist
+pdf("multitemp.pdf")
+par(mfrow=c(2,2))
+plotRGB(p224r63_1988, r=4, g=3, b=2, stretch="Lin")
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
+plotRGB(p224r63_1988, r=4, g=3, b=2, stretch="hist")
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="hist")
+dev.off()
 
