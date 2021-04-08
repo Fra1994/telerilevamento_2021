a7ed146daa8691fbf4bc5eadfb35752fd605fa91
Fra1994 <80578940+Fra1994@users.noreply.github.com>
2021-04-08 12:18:27 +0200

    Update R_code_times_series.r

--- R_code_times_series.r
+++ R_code_times_series.r
@@ -5,5 +5,32 @@
 # install.packages("raster")
 library(raster)
 
-setwd("~/lab/Greenland")
+setwd("c:/lab/Greenland")
 
+lst_2000 <- raster("2000.tif")
+lst_2005 <- raster("2005.tif")
+lst_2010 <- raster("2010.tif")
+lst_2015 <- raster("2015.tif")
+
+# par
+par(mfrow=c(2,2))
+plot(lst_2000)
+plot(lst_2005)
+plot(lst_2010)
+plot(lst_2015)
+
+# list of files:
+list.files()
+
+rlist <- list.files(pattern="tif")
+rlist
+
+import <- lapply(rlist,raster)
+import
+
+TGr <- stack(import)
+plot(TGr)
+
+plotRGB(TGr, 1, 2, 3, stretch="lin")
+plotRGB(TGr, 2, 3, 4, stretch="lin")
+plotRGB(TGr, 4, 3, 2, stretch="lin")
