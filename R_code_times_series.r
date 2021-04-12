f78a34f248e82ab69d34a67320dc2e519707fdce
Fra1994 <80578940+Fra1994@users.noreply.github.com>
2021-04-12 20:33:33 +0200

    Update R_code_times_series.r

--- R_code_times_series.r
+++ R_code_times_series.r
@@ -3,8 +3,11 @@
 # Data and code from Emanuela Cosma
 
 # install.packages("raster")
+# install.packages("rasterVis")
 library(raster)
+library(rasterVis)
 
+# library(rgdal)
 setwd("c:/lab/Greenland")
 
 lst_2000 <- raster("2000.tif")
@@ -22,15 +25,18 @@ plot(lst_2015)
 # list of files:
 list.files()
 
-rlist <- list.files(pattern="tif")
+rlist <- list.files(pattern="lst")
 rlist
 
 import <- lapply(rlist,raster)
 import
 
 TGr <- stack(import)
+TGr
 plot(TGr)
 
 plotRGB(TGr, 1, 2, 3, stretch="lin")
 plotRGB(TGr, 2, 3, 4, stretch="lin")
 plotRGB(TGr, 4, 3, 2, stretch="lin")
+
+levelplot(TGr)
