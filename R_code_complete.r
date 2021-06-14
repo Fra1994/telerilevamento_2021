52f6e2e5178b8c3a4b3e499d8e167c24b640b915
Fra1994 <80578940+Fra1994@users.noreply.github.com>
2021-06-14 13:49:37 +0200

    Update R_code_complete.r

--- R_code_complete.r
+++ R_code_complete.r
@@ -14,7 +14,7 @@
 # 8. R code Vegetetion indices
 # 9. R code Land cover
 # 10. R code variability 
-
+# 11. R code no2
 
 #------------------------------------------------
 
@@ -688,7 +688,80 @@ grid.arrange(p1, p2, p3, nrow = 1)
 
 #------------------------------------------------
 
+# 11. R code no2
 
+library(raster)
+library(RStoolbox) # here used for raster based multivariate analysis
+# 1. Set the working directory
+
+setwd("C:/lab/EN")
+
+# 2. Import the first image (single band)
+# we will select band 1, but the raster function enables to select other single-bands
+# [suggerimento studenti]
+# https://www.rdocumentation.org/packages/raster/versions/3.4-10/topics/raster
+ 
+EN01 <- raster("EN_0001.png")
+
+# 3. Plot the first imported image with your prefered Color Ramp Palette
+
+cls <- colorRampPalette(c("red","pink","orange","yellow")) (200)
+plot(EN01, col=cls)
+ 
+# 4. Import the last image (13th) and plot it with the previous Color Ram Palette
+
+EN13 <- raster("EN_0013.png")
+ 
+cls <- colorRampPalette(c("red","pink","orange","yellow")) (200)
+plot(EN13, col=cls)
+
+# 5. Make the difference between the two images and plot it
+# with March - January or January - March
+
+ENdif <- EN01 - EN13
+plot(ENdif, col=cls)
+
+# 6. Plot everything, altogether
+
+par(mfrow=c(3,1))
+plot(EN01, col=cls, main="NO2 in January")
+plot(EN13, col=cls, main="NO2 in March")
+plot(ENdif, col=cls, main="Difference (January - March)")
+
+# 7. Import the whole set
+
+# list of files:
+rlist <- list.files(pattern="EN")
+rlist
+
+import <- lapply(rlist,raster)
+import
+
+EN <- stack(import)
+plot(EN, col=cls)
+
+# 8. Replicate the plot of images 1 and 13 using the stack
+
+par(mfrow=c(2,1))
+plot(EN$EN_0001, col=cls)
+plot(EN$EN_0013, col=cls)
+ 
+# 9. Compute a PCA over the 13 images
+
+ENpca <- rasterPCA(EN)
+
+summary(ENpca$model)
+
+dev.off()
+plotRGB(ENpca$map, r=1, g=2, b=3, stretch="lin")
+
+# 10. Compute the local variability (local standard deviation) of the first PC
+
+PC1sd <- focal(ENpca$map$PC1, w=matrix(1/9, nrow=3, ncol=3, fun=sd)
+        
+plot(PC1sd, col=cls)       
+               
+#------------------------------------------------              
 
 
 
